0753ac60ef06fe131d6fffc5e4518da5885d8914
AkhilK004 <145592957+AkhilK004@users.noreply.github.com>
2024-06-05 13:17:13 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,12 @@
 
 function isAnagram(str1, str2) {
 
+   if (str1.length !== str2.length) {
+        return false;
+    }
+    let sortedStr1 = str1.split('').sort().join('');
+    let sortedStr2 = str2.split('').sort().join('');
+    return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
